2f963413d40afd4629d3576a86154b65b6891372
Nguyen Minh Tri <nmtri102work@gmail.com>
2026-03-03 02:22:13 +0700

    update UI

--- src/admin/ManageStaff.tsx
+++ src/admin/ManageStaff.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import "./ManageStaff.css";
 function ManageStaff() {
   const [users, setUsers] = useState<any[]>([]);
 
@@ -17,35 +17,49 @@ function ManageStaff() {
       .catch((err) => console.log(err));
   }, []);
 
-  return (
-    <div>
-      <h2>Danh sách nhân viên</h2>
+ return (
+  <div className="manage-wrapper">
+    <div className="manage-container">
+      <h2 className="page-title">Danh sách nhân viên</h2>
 
-      <table border={1} cellPadding={10}>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Tên</th>
-            <th>SĐT</th>
-            <th>Vai trò</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.username}</td>
-              <td>{user.full_name}</td>
-              <td>{user.phone_number}</td>
-              <td>{user.role}</td>
+      <div className="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Username</th>
+              <th>Tên</th>
+              <th>SĐT</th>
+              <th>Vai trò</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.username}</td>
+                <td>{user.full_name}</td>
+                <td>{user.phone_number}</td>
+                <td>
+                  <span
+                    className={
+                      user.role === "admin"
+                        ? "role-admin"
+                        : "role-staff"
+                    }
+                  >
+                    {user.role}
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default ManageStaff;
