ec2aa27337b5ba34bfae0c079f77855141cf1bda
Nguyen Minh Tri <nmtri102work@gmail.com>
2026-03-02 17:19:36 +0700

    fix css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,36 @@
-
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <h2 className="logo">NMT Mobile</h2>
-      <div className="menu">
-        <NavLink to="/" end>Trang chủ</NavLink>
-        <NavLink to="/booking">Đặt lịch</NavLink>
-        <NavLink to="/history">Lịch sử</NavLink>
-        <NavLink to="/about">Thông tin</NavLink>
+      <div className="nav-top">
+        <h2 className="logo">NMT Mobile</h2>
 
+        <div
+          className="hamburger"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          ☰
+        </div>
+      </div>
+
+      <div className={`menu ${isOpen ? "active" : ""}`}>
+        <NavLink to="/" end onClick={() => setIsOpen(false)}>
+          Trang chủ
+        </NavLink>
+        <NavLink to="/booking" onClick={() => setIsOpen(false)}>
+          Đặt lịch
+        </NavLink>
+        <NavLink to="/history" onClick={() => setIsOpen(false)}>
+          Lịch sử
+        </NavLink>
+        <NavLink to="/about" onClick={() => setIsOpen(false)}>
+          Thông tin
+        </NavLink>
       </div>
     </nav>
   );
