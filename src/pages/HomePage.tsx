286f5c676e77a6fd86d446dd76279b276a72ec3e
Nguyen Minh Tri <nmtri102work@gmail.com>
2026-03-03 23:09:47 +0700

    update UI Home Page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,25 @@
+import banner from "../assets/banner.png";
+import { useNavigate } from "react-router-dom";
+import "./HomePage.css";
+
 function HomePage() {
+  const navigate = useNavigate();
+
   return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
-      <h1>Chào mừng đến với NMT Fix</h1>
-      <p>Dịch vụ sửa chữa điện thoại chuyên nghiệp</p>
+    <div className="hero">
+      <img src={banner} alt="NMT Fix" className="hero-img" />
+
+      <div className="hero-overlay">
+        <h1>NMT FIX</h1>
+        <p>Dịch vụ sửa chữa điện tử chuyên nghiệp tại Tây Ninh</p>
+
+        <button
+          className="hero-btn"
+          onClick={() => navigate("/booking")}
+        >
+          Đặt lịch ngay
+        </button>
+      </div>
     </div>
   );
 }
