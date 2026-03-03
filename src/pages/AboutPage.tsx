24dd13041f64ad6ebef006907f305d09914500de
Nguyen Minh Tri <nmtri102work@gmail.com>
2026-03-03 23:26:10 +0700

    update UI Home Page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,24 +1,30 @@
+import banner from "../assets/banner.jpg";
 import "./AboutPage.css";
 
 function AboutPage() {
   return (
-    <div className="about-container">
-      <div className="about-card">
-        <h2>Thông tin cửa hàng</h2>
+    <div
+      className="about-container"
+      style={{ backgroundImage: `url(${banner})` }}
+    >
+      <div className="about-overlay">
+        <div className="about-card">
+          <h2>Thông tin cửa hàng</h2>
 
-        <div className="about-item">
-          <span>📍 Địa chỉ:</span>
-          <p>Ấp 10, Mỹ Lệ, Tây Ninh</p>
-        </div>
+          <div className="about-item">
+            <span>📍 Địa chỉ:</span>
+            <p>Ấp 10, Mỹ Lệ, Tây Ninh</p>
+          </div>
 
-        <div className="about-item">
-          <span>📞 SĐT:</span>
-          <p>0369 396 573</p>
-        </div>
+          <div className="about-item">
+            <span>📞 SĐT:</span>
+            <p>0369 396 573</p>
+          </div>
 
-        <div className="about-item">
-          <span>🛠 Dịch vụ:</span>
-          <p>Kinh doanh và sửa chữa các loại điện thoại và thiết bị điện tử</p>
+          <div className="about-item">
+            <span>🛠 Dịch vụ:</span>
+            <p>Kinh doanh và sửa chữa các loại điện thoại và thiết bị điện tử</p>
+          </div>
         </div>
       </div>
     </div>
