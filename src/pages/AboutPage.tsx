57fa4bb65eabf07b024e96a8e80279705061c4d8
Nguyen Minh Tri <nmtri102work@gmail.com>
2026-03-07 03:00:38 +0700

    cauhinhhethong

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,33 +1,47 @@
 import "./AboutPage.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function AboutPage() {
+
+  const [settings, setSettings] = useState<any>(null);
+
+  useEffect(() => {
+    axios
+      .get("https://nmt-mobile-backend.onrender.com/api/settings")
+      .then((res) => setSettings(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (!settings) return null;
+
   return (
-    <div
-      className="about-container"
-    >
+    <div className="about-container">
       <div className="about-overlay">
         <div className="about-card">
+
           <h2>Thông tin cửa hàng</h2>
 
           <div className="about-item">
             <span>📍 Địa chỉ:</span>
-            <p>Ấp 10, Mỹ Lệ, Tây Ninh</p>
+            <p>{settings.address}</p>
           </div>
 
           <div className="about-item">
             <span>📞 SĐT:</span>
-            <p>0369 396 573</p>
+            <p>{settings.phone}</p>
           </div>
 
           <div className="about-item">
-            <span>📞 Email:</span>
-            <p>support@nmtfix.com</p>
+            <span>📧 Email:</span>
+            <p>{settings.email}</p>
           </div>
 
           <div className="about-item">
             <span>🛠 Dịch vụ:</span>
-            <p>Kinh doanh và sửa chữa các loại điện thoại và thiết bị điện tử</p>
+            <p>{settings.description}</p>
           </div>
+
         </div>
       </div>
     </div>
